652e1cae1108f7a78cd94c9434916ac1c5f17af5
buaaxiaobaige <buaaxiaobaige@163.com>
2022-05-20 16:26:53 +0800

    tidy up

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-# import wandb
+import wandb
 from torch import optim
 from torch.utils.data import DataLoader, random_split
 from tqdm import tqdm
@@ -39,10 +39,10 @@ def train(net: UNet,
     train_loader = DataLoader(train_set, shuffle=True, **loader_args)
     val_loader = DataLoader(val_set, shuffle=False, drop_last=True, **loader_args)
 
-    # experiment = wandb.init(project='DRIVE', resume='allow', anonymous='must')
-    # experiment.config.update(dict(epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
-    #                               val_percent=val_percent, save_checkpoint=save_checkpoint, img_scale=img_scale,
-    #                               amp=amp))
+    experiment = wandb.init(project='DRIVE', resume='allow', anonymous='must')
+    experiment.config.update(dict(epochs=epochs, batch_size=batch_size, learning_rate=learning_rate,
+                                  val_percent=val_percent, save_checkpoint=save_checkpoint, img_scale=img_scale,
+                                  amp=amp))
     logging.info(f'''Starting training:
         Epochs:          {epochs}
         Batch size:      {batch_size}
@@ -93,38 +93,38 @@ def train(net: UNet,
                 pbar.update(images.shape[0])
                 global_step += 1
                 epoch_loss += loss.item()
-                # experiment.log({
-                #     'train loss': loss.item(),
-                #     'step': global_step,
-                #     'epoch': epoch
-                # })
+                experiment.log({
+                    'train loss': loss.item(),
+                    'step': global_step,
+                    'epoch': epoch
+                })
                 pbar.set_postfix(**{'loss (batch)': loss.item()})
 
                 division_step = (n_train // (10 * batch_size))
                 if division_step > 0:
                     if global_step % division_step == 0:
                         histograms = {}
-                        # for tag, value in net.named_parameters():
-                        #     tag = tag.replace('/', '.')
-                        #     histograms['Weights/' + tag] = wandb.Histogram(value.data.cpu())
-                        #     histograms['Gradients/' + tag] = wandb.Histogram(value.grad.data.cpu())
+                        for tag, value in net.named_parameters():
+                            tag = tag.replace('/', '.')
+                            histograms['Weights/' + tag] = wandb.Histogram(value.data.cpu())
+                            histograms['Gradients/' + tag] = wandb.Histogram(value.grad.data.cpu())
 
                         val_score = evaluate(net, val_loader, device)
                         scheduler.step(val_score)
 
                         logging.info('Validation Dice score: {}'.format(val_score))
-                        # experiment.log({
-                        #     'learning rate': optimizer.param_groups[0]['lr'],
-                        #     'validation Dice': val_score,
-                        #     'images': wandb.Image(images[0].cpu()),
-                        #     'masks': {
-                        #         'true': wandb.Image(masks[0].float().cpu()),
-                        #         'pred': wandb.Image(torch.softmax(masks_pred, dim=1).argmax(dim=1)[0].float().cpu()),
-                        #     },
-                        #     'step': global_step,
-                        #     'epoch': epoch,
-                        #     **histograms
-                        # })
+                        experiment.log({
+                            'learning rate': optimizer.param_groups[0]['lr'],
+                            'validation Dice': val_score,
+                            'images': wandb.Image(images[0].cpu()),
+                            'masks': {
+                                'true': wandb.Image(masks[0].float().cpu()),
+                                'pred': wandb.Image(torch.softmax(masks_pred, dim=1).argmax(dim=1)[0].float().cpu()),
+                            },
+                            'step': global_step,
+                            'epoch': epoch,
+                            **histograms
+                        })
         if save_checkpoint:
             Path(dir_checkpoint).mkdir(parents=True, exist_ok=True)
             torch.save(net.state_dict(), str(dir_checkpoint / 'checkpoint_epoch{}.pth'.format(epoch)))
@@ -149,11 +149,11 @@ if __name__ == '__main__':
     try:
         train(net=net,
               epochs=2,
-              batch_size=1,
-              learning_rate=1e-5,
+              batch_size=2,
+              learning_rate=1e-4,
               device=device,
               img_scale=1,
-              val_percent=0.1,
+              val_percent=0.2,
               amp=False)
     except KeyboardInterrupt:
         torch.save(net.state_dict(), 'INTERRUPTED.pth')
--- mytest.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
-    logging.info('test')
-    print('print')
