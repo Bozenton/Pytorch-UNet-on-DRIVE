e1bbd3de224a1af35b3ff2ce4941ca8d5ea2f6c8
buaaxiaobaige <buaaxiaobaige@163.com>
2022-05-28 23:12:28 +0800

    change readme

---
+++ utils/learnClass.py
@@ -0,0 +1,14 @@
+
+class building:
+    def __init__(self, h, w):
+        self.height = h
+        self.weight = w
+
+
+    def method(self, p):
+        return self.height + self.weight + p
+
+
+if __name__ == '__main__':
+    a = (1,2,3,4)
+    print(a[0])
